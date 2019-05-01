6a7855b4d1c4028a6ce42a4383af414f9e465a72
gandal123456 <ganeshgandal@gmail.com>
2019-05-01 13:50:31 +0530

    Third Commit

---
+++ src/test/java/testing/Test1.java
@@ -0,0 +1,13 @@
+package testing;
+
+import org.testng.annotations.Test;
+
+public class Test1
+{
+    @Test
+	public void Method()
+	{
+		System.out.println("Test1 runs");
+	}
+}
+
---
+++ src/test/java/testing/Test2.java
@@ -0,0 +1,13 @@
+package testing;
+
+import org.testng.annotations.Test;
+
+public class Test2 
+{
+	 @Test
+		public void Method4()
+		{
+			System.out.println("Test4 runs");
+		}
+
+}
